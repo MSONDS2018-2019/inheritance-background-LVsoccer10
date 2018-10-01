7565946d0767961b6932305fe20cf845eb7f1d22
Lucas Venetoulias <lucas@LV-MacBookPro.fios-router.home>
2018-09-30 22:07:01 -0400

    feat: Add subclasses to parent Tree class.

---
+++ src/main/java/ShortTree.java
@@ -0,0 +1,8 @@
+public class ShortTree extends Tree {
+
+	public ShortTree(int x, int y) {
+		super(x, y, "images/tree_2.png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/TallTree.java
@@ -0,0 +1,9 @@
+
+public class TallTree extends Tree {
+
+	public TallTree(int x, int y) {
+		super(x, y, "images/tree_1.png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
