a36e528e563dd626aa777cfe0e86fcab1f5878a2
Lucas Venetoulias <lucas@LV-MacBookPro.fios-router.home>
2018-09-30 23:05:40 -0400

    Add Stone and Fire as subclasses to Tile class. Place Fire and Stone tiles on board, both sides of eventual river.

---
+++ src/main/java/Fire.java
@@ -0,0 +1,9 @@
+
+public class Fire extends Tile {
+
+	public Fire(int i, int j) {
+		super(i, j, "images/fire.png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/Stone.java
@@ -0,0 +1,9 @@
+
+public class Stone extends Tile {
+
+	public Stone(int i, int j) {
+		super(i, j, "images/stone.png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
